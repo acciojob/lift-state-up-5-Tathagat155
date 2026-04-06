46202942e2842b3c28cf7894ecbc3016fbe3f94b
Tathagat155 <tathagatkumar70618@gmail.com>
2026-04-06 17:05:40 +0530

    Update Child.js

--- src/components/Child.js
+++ src/components/Child.js
@@ -2,13 +2,16 @@ import react from 'react'
 const Child = ({isLoggedIn,setLogin}) => {
   return (
    <div>
-  {isLoggedIn: <div>You are logged in!</div> ?<div>
+  {isLoggedIn: (<div>You are logged in!</div> )?(<div>
      <form>
       <div>Username:<input type='text' ></div>
       <div>Password:<input type="password"></div>
-     <div><button onClick={()=>setLogin(true)}>Login</button>
+     <div><button onClick={(e)=>{
+       e.preventDefault();
+       setLogin(true)
+     }}>Login</button>
     </form>
-    </div>}
+    </div>)}
     </div>
     
   )
