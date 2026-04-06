05ff4739d0b2d4172a5c30dc0e5b2c8f0b53b418
Tathagat155 <tathagatkumar70618@gmail.com>
2026-04-06 16:55:14 +0530

    Update Child.js

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,1 +1,17 @@
+import react from 'react'
+const Child = ({isLoggedIn,setLogin}) => {
+  return (
+   <div>
+  {isLoggedIn: <div>You are logged in!</div> ?<div>
+     <form>
+      <div>Username:<input type='text' ></div>
+      <div>Password:<input type="password"></div>
+     <div><button onClick={()=>setLogin(true)}>Login</button>
+    </form>
+    </div>}
+    </div>
+    
+  )
+}
 
+export default Child
